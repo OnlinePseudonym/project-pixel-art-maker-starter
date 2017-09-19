b4ff5c32b9669c48b3b2ce8ad3479f1d07896d09
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-09-19 16:39:30 -0700

    designed table and add comments

--- designs.js
+++ designs.js
@@ -3,17 +3,37 @@
 
 // When size is submitted by the user, call makeGrid()
 
+let color = "#000000";
+
+//Change the value of color depending on the users input
 $('#colorPicker').change(function() {
-  let color = $(this).val();
+  color = $(this).val();
 });
 
-$('#sizePicker').submit(function() {
+//clear and create new grid based on user inputs when submit button is clicked
+//allow users to click a data cell to alter it's background to the desired color.
+$('#sizePicker').submit(function(evt) {
+  evt.preventDefault();
+  clearGrid();
   const height = $('#input_height').val();
-  const width = $('input_width').val();
+  const width = $('#input_width').val();
+  makeGrid(height,width);
+  $('td').click(function() {
+    $(this).css("background-color",color);
+  });
 });
 
-function makeGrid() {
+//function to clear grid
+function clearGrid() {
+  $('tr').remove();
+}
 
-// Your code goes here!
-
+//function to create grid with inputs for height and weight.
+function makeGrid(h,w) {
+  for (let rows = 0; rows < h; rows++) {
+    $('#pixel_canvas').append('<tr></tr>');
+    for (let columns = 0; columns < w; columns++) {
+      $('tr').last().append('<td></td>');
+    };
+  }
 }
